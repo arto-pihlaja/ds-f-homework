28ff4ebb79f1a9e9e690f81d91d1f348268bdd67
Pihlaja Arto <arto.pihlaja@bilot.fi>
2018-07-12 23:06:35 +0300

    first working build

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import request
+from flask import request, json, Response
 from app import app
 import pickle
+import pandas as pd
 
 @app.route("/")
 @app.route("/index")
@@ -9,25 +10,29 @@ def index():
 @app.route("/predict", methods=["GET","POST"])
 def predict():
     if request.method == "GET":
-        pass 
-    else:        
-        return create_response(request)
+        return "Please use POST!"
+    else:                               
+        #resp = str(request.json["crime_rate"])
+        resp = create_response(request)
+        return Response(resp, status=200, mimetype="application/json")
+        #return request_json #["crime_rate"]
 
 def create_response(request):
-    from flask import jsonify
-    request_json = request.get_json()
-    independent_vars = ['crime_rate' 'avg_number_of_rooms' 'distance_to_employment_centers'
-                'property_tax_rate' 'pupil_teacher_ratio']
+    #return "Processing request!"
+    independent_vars = ['crime_rate', 'avg_number_of_rooms', 'distance_to_employment_centers',
+                'property_tax_rate', 'pupil_teacher_ratio']
     iv_list = []
     for iv in independent_vars:
         try:
-            iv_list.append(request_json[iv])
+            iv_list.append(request.json[iv])
         except RuntimeError:
             #return Response("Bad request", status = 400)
-            return "Bad request"
-#        crime_rate = independent_vars["crime_rate"]
-#        avg_number_of_rooms = independent_vars["avg_number_of_rooms"]
-    model = pickle.load("cv_polyfit.pkl")
-    response = {"house_value" : model.predict(iv_list),
-        "stddev" : model.stddev }
-    return jsonify(response)    
+            return "Bad request"    
+# #        crime_rate = independent_vars["crime_rate"]
+# #        avg_number_of_rooms = independent_vars["avg_number_of_rooms"]
+    model = pickle.load(open(".\\app\\cv_polyfit.pkl","rb"))
+    rmse = pickle.load(open(".\\app\\cv_rmse.pkl","rb"))
+    df = pd.DataFrame(iv_list).transpose()
+    response = {"house_value" : model.predict(df)[0],
+        "stddev" : rmse }
+    return json.dumps(response)    
