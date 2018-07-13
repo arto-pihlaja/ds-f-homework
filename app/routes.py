d6b2644ade697df9461564125b7643c1b7e3d660
Pihlaja Arto <arto.pihlaja@bilot.fi>
2018-07-13 07:35:26 +0300

    Cleansed code

--- app/routes.py
+++ app/routes.py
@@ -12,13 +12,10 @@ def predict():
     if request.method == "GET":
         return "Please use POST!"
     else:                               
-        #resp = str(request.json["crime_rate"])
         resp = create_response(request)
         return Response(resp, status=200, mimetype="application/json")
-        #return request_json #["crime_rate"]
 
 def create_response(request):
-    #return "Processing request!"
     independent_vars = ['crime_rate', 'avg_number_of_rooms', 'distance_to_employment_centers',
                 'property_tax_rate', 'pupil_teacher_ratio']
     iv_list = []
@@ -26,10 +23,7 @@ def create_response(request):
         try:
             iv_list.append(request.json[iv])
         except RuntimeError:
-            #return Response("Bad request", status = 400)
             return "Bad request"    
-# #        crime_rate = independent_vars["crime_rate"]
-# #        avg_number_of_rooms = independent_vars["avg_number_of_rooms"]
     model = pickle.load(open(".\\app\\cv_polyfit.pkl","rb"))
     rmse = pickle.load(open(".\\app\\cv_rmse.pkl","rb"))
     df = pd.DataFrame(iv_list).transpose()
